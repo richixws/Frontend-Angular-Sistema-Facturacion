c369bc622240932397b8eecda4099c5ec8d1710d
richard wong <rwongsol@hotmail.com>
2020-08-04 17:24:31 -0500

    "cambios en componente html"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'clientes-app';
+  title = 'vienvenido a angular';
+  curso:string = 'spring 5 con angular 7';
+  profesor:string = 'andres guzman';
 }
