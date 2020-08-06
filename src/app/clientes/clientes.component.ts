df564b7817a2175d1eb87ec9134c8c149895d9cd
richard wong <rwongsol@hotmail.com>
2020-08-06 10:04:50 -0500

    creacion de las clases cliente

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Cliente} from './cliente';
+import {CLIENTES} from './clientes.json';
+
 
 @Component({
   selector: 'app-clientes',
-  templateUrl: './clientes.component.html',
-  styleUrls: ['./clientes.component.css']
+  templateUrl: './clientes.component.html'
 })
 export class ClientesComponent implements OnInit {
+
+  clientes:Cliente[];
 
   constructor() { }
 
   ngOnInit(): void {
+
+    this.clientes=CLIENTES;
   }
 
 }
---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,10 @@
+import { Cliente} from './cliente';
+
+
+
+
+export const CLIENTES:Cliente[] = [
+                      {id:1,nombre:'richard',apellido:'wong',createAt:'2020-12-11',email:'rwongsol@gmail.com'},
+                      {id:2,nombre:'paul',apellido:'wong',createAt:'2020-12-11',email:'paulwong@gmail.com'},
+                      {id:3,nombre:'kelly',apellido:'wong',createAt:'2020-12-11',email:'kelly@gmail.com'}
+                    ];
