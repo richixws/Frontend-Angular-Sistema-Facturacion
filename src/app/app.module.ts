68a7aef892450ea2fb0003eac9ff6c570f0baaf1
richard wong <rwongsol@hotmail.com>
2020-08-24 22:36:07 -0500

    implementando service angular con httpclient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import {FooterComponent} from './footer/footer.component';
 import { ClientesComponent } from './clientes/clientes.component';
 import {ClienteService} from './clientes/cliente.service';
 import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
+
 
 const routes: Routes = [
   { path: '', redirectTo: '/clientes', pathMatch:'full' },
@@ -25,6 +28,7 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     RouterModule.forRoot(routes)
   ],
   providers: [ClienteService],
--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import {CLIENTES} from './clientes.json';
 import { Cliente} from './cliente';
-import { Observable,of } from 'rxjs';
+import { Observable,of} from 'rxjs';
+import { HttpClient} from '@angular/common/http';
+import {map} from 'rxjs/operators';
 
 
 
@@ -10,12 +12,18 @@ import { Observable,of } from 'rxjs';
 @Injectable()
 export class ClienteService {
 
-  constructor() { }
+  private urlEndPoint:string= 'http://localhost:8080/api/clientes';
+
+  constructor(private http:HttpClient) { }
 
   //metodo getClientes para devolver el listado de cliente de json.
   getClientes():Observable<Cliente[]>{
 
-    return of(CLIENTES);
+    return this.http.get(this.urlEndPoint).pipe(
+
+      map((response) => response as Cliente[])
+    );
+  //  return of(CLIENTES);
 
   }
 }
