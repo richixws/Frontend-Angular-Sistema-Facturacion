5719179f5985fab3cebe8d6d1823d969fd04c0bd
richard wong <rwongsol@hotmail.com>
2020-09-11 09:56:48 -0500

    formatear fecha y uppercase en las plantillas html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {HeaderComponent} from './header/header.component';
@@ -41,7 +41,7 @@ const routes: Routes = [
     FormsModule,
     RouterModule.forRoot(routes)
   ],
-  providers: [ClienteService],
+  providers: [ClienteService, {provide: LOCALE_ID, useValue: 'es'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -34,7 +34,7 @@ export class ClienteService {
       return clientes.map( cliente=>{
           cliente.nombre=cliente.nombre.toUpperCase();
           let datePipe=new DatePipe('es');
-          cliente.createAt=datePipe.transform(cliente.createAt,'EEEE dd, MMMM yyyy');
+        //  cliente.createAt=datePipe.transform(cliente.createAt,'EEEE dd, MMMM yyyy');
         //  cliente.createAt=formatDate(cliente.createAt,'dd/MM/yyyy','en-US');
           return cliente;
       });
