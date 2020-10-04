9e038e0a310f3f3abc2ece60c81e8917caf842de
richard wong <rwongsol@hotmail.com>
2020-10-03 21:50:40 -0500

    implementacion de subida de archivo

--- src/app/clientes/detalle/detalle.component.ts
+++ src/app/clientes/detalle/detalle.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { from } from 'rxjs';
+
+import swal from 'sweetalert2';
 import { Cliente } from '../cliente';
 import {ClienteService} from '../cliente.service';
 
@@ -13,16 +15,17 @@ export class DetalleComponent implements OnInit {
 
   cliente:Cliente;
   titulo: string="detalle de cliente";
+  private fotoSeleccionada: File;
 
   constructor(private clienteService:ClienteService, private activatedRoute:ActivatedRoute ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
 
     this.activatedRoute.paramMap.subscribe(params =>{
 
       let id:number= +params.get('id');
       if(id){
-        this.clienteService.getClientes(id).subscribe(cliente=>{
+        this.clienteService.getCliente(id).subscribe(cliente=>{
 
           this.cliente=cliente;
 
@@ -32,5 +35,23 @@ export class DetalleComponent implements OnInit {
 
 
   }
+   seleccionarFoto(event){
+
+     this.fotoSeleccionada=event.target.files[0];
+     console.log(this.fotoSeleccionada);
+
+   }
+
+   subirFoto(){
+
+     this.clienteService.subirFoto(this.fotoSeleccionada,this.cliente.id)
+     .subscribe(cliente =>{
+        
+         this.cliente=cliente;
+         swal.fire('la foto se a subido correctamente!',`la foto se a subido con exito: ${this.cliente.foto}`,'success');
+     })
+
+   }
+
 
 }
