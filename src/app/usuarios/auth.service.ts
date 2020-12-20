3bad94f6fa072d3868222eed796e624b6cc690a6
richard wong <rwongsol@hotmail.com>
2020-12-20 12:30:08 -0500

    guardando datos del token y usuario en el cessionstore

--- src/app/usuarios/auth.service.ts
+++ src/app/usuarios/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import {HttpClient, HttpHeaders}  from '@angular/common/http';
 import {Usuario} from './usuario'
+import { JAN } from '@angular/material/core';
 
 @Injectable({
   providedIn: 'root'
@@ -9,18 +10,54 @@ import {Usuario} from './usuario'
 
 export class AuthService {
 
+ private _usuario:Usuario;
+ private _token: string;
 
- constructor(private http:HttpClient) {
-   
+
+
+ constructor(private http:HttpClient) {   
+
+ }
+
+ public get usuario():Usuario{
+     
+  if(this._usuario!=null){
+    return this._usuario;
+
+  }else if(this._usuario==null && sessionStorage.getItem('usuario')!=null){
+
+    this._usuario=JSON.parse(sessionStorage.getItem('usuario')) as Usuario;
+    return this._usuario;
   }
+
+  return new Usuario();
+
+ }
+ public get token(): string{
+
+  if(this._token!=null){
+    return this._token;
+
+  }else if(this._token==null && sessionStorage.getItem('token')!=null){
+
+    this._token=sessionStorage.getItem('token');
+    return this._token;
+  }
+
+   return null;
+ }
+
+
 
   login(usuario:Usuario):Observable<any>{
 
     const urlEndpoint='http://localhost:8080/oauth/token';
 
-    const credenciales=btoa('angularapp'+':'+'12345');
+    const credenciales=btoa('angularapp' + ' : ' + '12345');
 
-    const httpHeaders=new HttpHeaders({'Content-Type':'application/x-www-form-urlencoded','Authorization':'Basic'+credenciales});
+    const httpHeaders=new HttpHeaders({
+      'Content-Type':'application/x-www-form-urlencoded',
+      'Authorization':'Basic'+credenciales});
 
     let params=new URLSearchParams();
 
@@ -29,7 +66,38 @@ export class AuthService {
     params.set('password',usuario.password);
     
     console.log(params.toString());
+
     return this.http.post<any>(urlEndpoint,params.toString(),{headers: httpHeaders})
   
   }
+
+  guardarUsuario(accessToken:string ):void{
+
+     let payload=this.obtenerDatosToken(accessToken);
+    this._usuario=new Usuario()
+    this._usuario.nombre=payload.nombre;
+    this._usuario.apellido=payload.apellido;
+    this._usuario.email=payload.email;
+    this._usuario.username=payload.user_name;
+    this._usuario.roles=payload.authorities;
+
+    sessionStorage.setItem('usuario', JSON.stringify(this._usuario));
+
+  }
+  guardarToken(accessToken:string):void{
+
+    this._token=accessToken;
+    sessionStorage.setItem('token',accessToken);
+
+  }
+
+  obtenerDatosToken(accessToken: string):any{
+       
+    if(accessToken!=null){
+      return JSON.parse(atob(accessToken.split(".")[1]));
+    }
+    return null;
+
+  }
+
 }
--- src/app/usuarios/login.component.ts
+++ src/app/usuarios/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import swal from 'sweetalert2';
 import {Usuario} from './usuario';
+import {AuthService} from './auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,7 +13,8 @@ export class LoginComponent implements OnInit {
  titulo:string='Por favor Sign In!';
  usuario:Usuario;
 
-  constructor() { 
+  constructor( private authService:AuthService, private router:Router) {
+
 
     this.usuario=new Usuario();
   }
@@ -26,6 +29,17 @@ export class LoginComponent implements OnInit {
       return; 
 
     }
+    this.authService.login(this.usuario).subscribe(response=>{
+      console.log(response);
+
+      this.authService.guardarUsuario(response.access_token);
+      this.authService.guardarToken(response.access_token);
+
+      let usuario=this.authService.usuario;
+
+      this.router.navigate(['/clientes']);
+      swal.fire('Login',`Hola ${usuario.username},has iniciado sesion con exito`,'success');
+    });
   }
 
 }
